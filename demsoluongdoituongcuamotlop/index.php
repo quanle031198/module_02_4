e314355a0040cb17354733f420add3937996abd0
quanle031198 <79827065+quanle031198@users.noreply.github.com>
2021-04-07 11:35:55 +0700

    dem so luong doi tuong cua mot lop

---
+++ demsoluongdoituongcuamotlop/index.php
@@ -0,0 +1,28 @@
+<?php
+class Application {
+    private $name;
+    public static $count = 0;
+    public static $count1 = 5;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+        self::$count++;
+    }
+
+    public function __toString()
+    {
+        return 'Application name: '. $this->name;
+    }
+}
+
+echo "Total objects count: ". Application::$count ."<br/>";
+$app1 = new Application('App One');
+echo "Total objects count: ". Application::$count ."<br/>";
+$app2 = new Application('App Two');
+echo "Total objects count: ". Application::$count ."<br/>";
+$app3 = new Application('App Three');
+echo "Total objects count: ". Application::$count ."<br/>";
+echo $app1 .'<br/>';
+echo $app2 .'<br/>';
+echo $app3 .'<br/>';
